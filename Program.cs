38d4ba9abd8d855f92630759a12a6db183ae3ab1
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-02 23:21:10 +0300

    Add file template

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace gb;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    public static void Main()
+    {
+        
+    }
+}
